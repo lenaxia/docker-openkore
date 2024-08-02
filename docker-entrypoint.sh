f8a3670461f096c2b49dfc63d2847495befcdcdc
Lenaxia <github@47north.lat>
2024-08-02 04:47:38 +0000

    blah

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -37,7 +37,7 @@ if [ -z "${OK_USERNAME}" ]; then echo "Missing OK_USERNAME environment variable.
 if [ -z "${OK_PWD}" ]; then echo "Missing OK_PWD environment variable. Unable to continue."; exit 1; fi
 if [ -z "${OK_CHAR}" ]; then OK_CHAR=1; fi
 
-if [ "${OK_KILLSTEAL}" = "1" ]; then 
+if [ "${OK_KILLSTEAL}" = "1" ]; then
     sed -i "1507s|return 0|return 1|" /opt/openkore/src/Misc.pm
     sed -i "1534s|return 0|return 1|" /opt/openkore/src/Misc.pm
     sed -i "1571s|return !objectIsMovingTowardsPlayer(\$monster);|return 1;|" /opt/openkore/src/Misc.pm
@@ -51,6 +51,7 @@ else
     if [ -z "${MYSQL_DB}" ]; then echo "Missing MYSQL_DB environment variable. Unable to continue."; exit 1; fi
     if [ -z "${MYSQL_USER}" ]; then echo "Missing MYSQL_USER environment variable. Unable to continue."; exit 1; fi
     if [ -z "${MYSQL_PWD}" ]; then echo "Missing MYSQL_PWD environment variable. Unable to continue."; exit 1; fi
+    AVAILABLE_USERNAMES=()
     for i in `seq 0 ${OK_USERNAMEMAXSUFFIX}`;
     do
         USERNAME=${OK_USERNAME}${i}
@@ -59,64 +60,69 @@ else
         if [ "${CHAR_IS_ONLINE}" == "0" ]; then
             MYSQL_QUERY="UPDATE \`char\` SET \`online\`=1 WHERE name='${USERNAME}'"
             mysql -u${MYSQL_USER} -p${MYSQL_PWD} -h ${MYSQL_HOST} -D ${MYSQL_DB} -ss -e "${MYSQL_QUERY}"
-            CLASS=$(mysql -u${MYSQL_USER} -p${MYSQL_PWD} -h ${MYSQL_HOST} -D ${MYSQL_DB} -ss -e "SELECT class FROM \`char\` WHERE name='${USERNAME}';")
-            case ${CLASS} in
-                4) # ACOLYTE
-                    mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
-                    cp /opt/openkore/control/class/acolyte.txt /opt/openkore/control/config.txt
-                    ;;
-                8) # PRIEST
-                    mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
-                    cp /opt/openkore/control/class/priest.txt /opt/openkore/control/config.txt
-                    ;;
-                15) # MONK
-                    mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
-                    cp /opt/openkore/control/class/monk.txt /opt/openkore/control/config.txt
-                    ;;
-                2) # MAGE
-                    mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
-                    cp /opt/openkore/control/class/mage.txt /opt/openkore/control/config.txt
-                    ;;
-                9) # WIZARD
-                    mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
-                    cp /opt/openkore/control/class/wizard.txt /opt/openkore/control/config.txt
-                    ;;
-                16) # SAGE
-                    mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
-                    cp /opt/openkore/control/class/sage.txt /opt/openkore/control/config.txt
-                    ;;
-                1) # SWORDMAN
-                    mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
-                    cp /opt/openkore/control/class/swordman.txt /opt/openkore/control/config.txt
-                    ;;
-                7) # KNIGHT
-                    mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
-                    cp /opt/openkore/control/class/knight.txt /opt/openkore/control/config.txt
-                    ;;
-            esac
-            sed -i "s|^username$|username ${USERNAME}|g" /opt/openkore/control/config.txt
-            # 1,2 -> Follow OK_FOLLOW_USERNAME1, 3,4 -> Follow OK_FOLLOW_USERNAME2, 5-10 -> Do not follow
-            case $(shuf -i1-10 -n1) in
-                1|2)
-                    if ! [ -z "${OK_FOLLOW_USERNAME1}" ]; then
-                        sed -i "s|^followTarget$|followTarget ${OK_FOLLOW_USERNAME1}|g" /opt/openkore/control/config.txt
-                        #sed -i "s|^attackAuto 2$|attackAuto 1|g" /opt/openkore/control/config.txt
-                    fi
-                    ;;
-                3|4)
-                    if ! [ -z "${OK_FOLLOW_USERNAME2}" ]; then
-                        sed -i "s|^followTarget$|followTarget ${OK_FOLLOW_USERNAME2}|g" /opt/openkore/control/config.txt
-                        #sed -i "s|^attackAuto 2$|attackAuto 1|g" /opt/openkore/control/config.txt
-                    fi
-                    ;;
-            esac
-            break
+            AVAILABLE_USERNAMES+=("${USERNAME}")
         fi
     done
+    if [ ${#AVAILABLE_USERNAMES[@]} -eq 0 ]; then
+        echo "No available usernames found. Exiting."
+        exit 1
+    fi
+    SELECTED_USERNAME=${AVAILABLE_USERNAMES[$((RANDOM % ${#AVAILABLE_USERNAMES[@]}))]}
+    CLASS=$(mysql -u${MYSQL_USER} -p${MYSQL_PWD} -h ${MYSQL_HOST} -D ${MYSQL_DB} -ss -e "SELECT class FROM \`char\` WHERE name='${SELECTED_USERNAME}';")
+    case ${CLASS} in
+        4) # ACOLYTE
+            mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
+            cp /opt/openkore/control/class/acolyte.txt /opt/openkore/control/config.txt
+            ;;
+        8) # PRIEST
+            mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
+            cp /opt/openkore/control/class/priest.txt /opt/openkore/control/config.txt
+            ;;
+        15) # MONK
+            mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
+            cp /opt/openkore/control/class/monk.txt /opt/openkore/control/config.txt
+            ;;
+        2) # MAGE
+            mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
+            cp /opt/openkore/control/class/mage.txt /opt/openkore/control/config.txt
+            ;;
+        9) # WIZARD
+            mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
+            cp /opt/openkore/control/class/wizard.txt /opt/openkore/control/config.txt
+            ;;
+        16) # SAGE
+            mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
+            cp /opt/openkore/control/class/sage.txt /opt/openkore/control/config.txt
+            ;;
+        1) # SWORDMAN
+            mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
+            cp /opt/openkore/control/class/swordman.txt /opt/openkore/control/config.txt
+            ;;
+        7) # KNIGHT
+            mv /opt/openkore/control/config.txt /opt/openkore/control/config.txt.bak
+            cp /opt/openkore/control/class/knight.txt /opt/openkore/control/config.txt
+            ;;
+    esac
+    sed -i "s|^username.*|username ${SELECTED_USERNAME}|g" /opt/openkore/control/config.txt
+    # 1,2 -> Follow OK_FOLLOW_USERNAME1, 3,4 -> Follow OK_FOLLOW_USERNAME2, 5-10 -> Do not follow
+    case $(shuf -i1-10 -n1) in
+        1|2)
+            if ! [ -z "${OK_FOLLOW_USERNAME1}" ]; then
+                sed -i "s|^followTarget$|followTarget ${OK_FOLLOW_USERNAME1}|g" /opt/openkore/control/config.txt
+                #sed -i "s|^attackAuto 2$|attackAuto 1|g" /opt/openkore/control/config.txt
+            fi
+            ;;
+        3|4)
+            if ! [ -z "${OK_FOLLOW_USERNAME2}" ]; then
+                sed -i "s|^followTarget$|followTarget ${OK_FOLLOW_USERNAME2}|g" /opt/openkore/control/config.txt
+                #sed -i "s|^attackAuto 2$|attackAuto 1|g" /opt/openkore/control/config.txt
+            fi
+            ;;
+    esac
 fi
 sed -i "s|^master$|master ${OK_SERVER}|g" /opt/openkore/control/config.txt
-sed -i "s|^server\s*\(.*\)$|server 0|g" /opt/openkore/control/config.txt
-sed -i "s|^password$|password ${OK_PWD}|g" /opt/openkore/control/config.txt
+sed -i "s|^server.*|server 0|g" /opt/openkore/control/config.txt
+sed -i "s|^password.*|password ${OK_PWD}|g" /opt/openkore/control/config.txt
 sed -i "s|^char$|char ${OK_CHAR}|g" /opt/openkore/control/config.txt
 sed -i "s|^autoResponse 0$|autoResponse 1|g" /opt/openkore/control/config.txt
 sed -i "s|^autoResponseOnHeal 0$|autoResponseOnHeal 1|g" /opt/openkore/control/config.txt
